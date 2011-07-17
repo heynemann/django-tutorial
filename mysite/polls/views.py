7e623800ffd43812b9eeb10c536c7937ac4aeb9a
Bernardo Heynemann <heynemann@gmail.com>
2011-07-17 01:52:57 -0300

    End of part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.template import Context, loader
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
+from polls.models import Poll
+
+# ...
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    t = loader.get_template('polls/index.html')
+    c = Context({
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(t.render(c))
+
+def detail(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,13 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^mysite/', include('mysite.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
